19f9969871a4fde3fc7a017c63ff1b4f334ea469
prodiegus <diego.d.a.f.v.2000@gmail.com>
2021-08-26 20:57:21 -0400

    Vista y controllador de la ventana de inicio
    primera version de la vista y controlador de la vista

--- Kriegspantzer/src/Kriegspantzer.java
+++ Kriegspantzer/src/Kriegspantzer.java
@@ -18,10 +18,17 @@ public class Kriegspantzer extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("IniciarJuegoView.fxml"));
-        
+        FXMLLoader loader =new FXMLLoader(getClass().getResource("IniciarJuegoView.fxml"));
+
+        Parent root = loader.load();
         Scene scene = new Scene(root);
-        
+
+        IniciarJuegoViewController controller = loader.getController();
+
+        String[] colors = {"Azul", "Verde", "Amarillo", "Rojo", "Morado", "Naranja"};
+
+        controller.setBoxes(colors);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
