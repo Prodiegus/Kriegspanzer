f9d7b834529861be56aa9c4932ee8e25aa7ae744
Omar Muñoz <omar25092001@gmail.com>
2021-08-26 21:19:29 -0400

    Pseudo codigo inicial Proyecto

---
+++ Kriegspantzer/src/Bala.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pseudo.java;
+
+/**
+ *
+ * @author omaii
+ */
+public class Bala extends Tanque {
+    private int [] pos_bala;
+    private boolean estado_bala;
+    private double velocidad;
+    private double angulo;
+}
+
+public Bala(int pos_bala, double velocidad, double angulo){
+}
