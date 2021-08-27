fe17f56946f81523bfa8321de0a49b016488710b
Omar Muñoz <omar25092001@gmail.com>
2021-08-26 21:37:40 -0400

    Auto stash before merge of "Omarcito" and "origin/Proyecto"
    Actulice mi codigo UWU

--- Kriegspantzer/src/Juego.java
+++ Kriegspantzer/src/Juego.java
@@ -1,13 +1,36 @@
+<<<<<<< Updated upstream
 import java.util.ArrayList;
 
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pseudo.java;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author omaii
+ */
+>>>>>>> Stashed changes
 public class Juego {
     private ArrayList<Jugador> jugadores;
     private boolean estadoJuego;
     private String ganador;
     
+<<<<<<< Updated upstream
     public Juego(ArrayList<Jugador> jugadores){
         this.jugadores = jugadores;
     }
 }
 
 
+=======
+}
+
+public Juego(ArrayList<Jugador> jugadores){
+}
+>>>>>>> Stashed changes
