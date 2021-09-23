ee01f8d8db34d4e05990f657b5de9aca9f72d9c5
prodiegus <diego.d.a.f.v.2000@gmail.com>
2021-09-22 21:13:38 -0300

    Proyecto v5

--- Kriegspantzer/src/IniciarJuegoViewController.java
+++ Kriegspantzer/src/IniciarJuegoViewController.java
@@ -11,15 +11,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.ImageCursor;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -32,7 +29,6 @@ public class IniciarJuegoViewController implements Initializable {
     @FXML private TextField nJugador2;
 
     //label de testeo
-    @FXML private Label mouseLb;
     private int map;
     private Mapa mapa;
     
@@ -116,51 +112,6 @@ public class IniciarJuegoViewController implements Initializable {
             JOptionPane.showMessageDialog(null, "Error: 006\nno se a podido cargar el juego");
         }
         return true;
-    }
-    
-    @FXML
-    private void handleEdMap(ActionEvent event){
-        //la instruccion esta dentro de un try catch debido a que se podrian presentar errores en la ejecucion 
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("EditorMapa.fxml"));
-
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-
-            EditorMapaController controller = loader.getController();
-
-            Image img = new Image("img/Cursor32x32.png");
-            scene.setCursor(new ImageCursor(img));
-            controller.setMap(map+1);
-            controller.iniciarMapa();
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.setResizable(false);
-            stage.setTitle("Kriegspanzer Map Editor");
-            stage.getIcons().add(new Image(getClass().getResourceAsStream("img/icon.png")));
-            stage.setScene(scene);
-            stage.show();
-
-        } catch (IOException e) {
-            //en caso de que algo salga mal mostraremos el siguiente mensaje
-            JOptionPane.showMessageDialog(null, "Error 001:\nNo ha sido posible cargar el editor\n"+e.getCause());
-        }
-        
-    }
-
-    //metodo de testeo
-    @FXML
-    private void handleMouseMove(MouseEvent event){
-        //mapaPanel.autosize();
-        double alto = mapaPanel.getHeight();
-        double ancho = mapaPanel.getWidth();
-        double altoI = mapaPanel.getPrefHeight();
-        double anchoI = mapaPanel.getPrefWidth();
-        double altoScale = ancho/anchoI;
-        double anchoScale = alto/altoI;
-        mouseLb.setText("Mouse pos: ("+(int)(event.getX()/altoScale)+"<-->"+(int)(event.getY()/anchoScale)+
-                        ")\nPanel size: "+ancho+"X"+alto+
-                        "\nPanel Scale: "+altoScale+"X"+anchoScale);
     }
     //closer
     @FXML private void close(ActionEvent event) {
